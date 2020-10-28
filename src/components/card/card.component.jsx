0065163ff510de2796e8ce6b5be6d610e3bf723b
neutral-Stage <anirbanroy691@gmail.com>
2020-10-29 06:39:05 +1100

    adding file for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) =>(
+    <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt=""/>
+        <h2>
+            {props.monster.name}
+        </h2>
+        <p>{props.monster.email}</p>
+    </div>
+)
